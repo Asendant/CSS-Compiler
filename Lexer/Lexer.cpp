7e135205314c35fcfe482d7b6c1387078404d641
Jacob Althen <jalthen@calpoly.edu>
2025-12-30 16:05:43 -0800

    Added lexer that can read a file's contents

--- Lexer/Lexer.cpp
+++ Lexer/Lexer.cpp
@@ -1,8 +1,23 @@
-//
-// Created by jacob on 12/30/2025.
-//
-
 #include "Lexer.h"
+#include <iostream>
+#include <fstream>
 
 namespace Lexer {
-} // Lexer
+    Lexer::Lexer() {
+        const std::string pathName = "C:/Users/jacob/Desktop/test.txt";
+
+        ReadFromFile(&pathName);
+    }
+
+    void Lexer::ReadFromFile(const std::string *pathToSourceFile) {
+        std::string sourceCode;
+
+        std::ifstream sourceFile(*pathToSourceFile);
+
+        getline(sourceFile, sourceCode);
+
+        std::cout << sourceCode << std::endl;
+
+        sourceFile.close();
+    }
+}
--- Lexer/Lexer.h
+++ Lexer/Lexer.h
@@ -1,8 +1,13 @@
 #ifndef COMPILER_LEXER_H
 #define COMPILER_LEXER_H
+#include <string>
 
 namespace Lexer {
     class Lexer {
+    public:
+        Lexer();
+    private:
+        static void ReadFromFile(const std::string *pathToSourceFile);
     };
 }
 
